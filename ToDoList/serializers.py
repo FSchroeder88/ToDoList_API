3332f80b67a86d7b2d8ac1ebf5c4023b4662f301
FSchroeder88 <96183878+FSchroeder88@users.noreply.github.com>
2022-09-13 17:21:36 +0200

    ToDo ready

--- ToDoList/serializers.py
+++ ToDoList/serializers.py
@@ -1,8 +1,17 @@
+from dataclasses import fields
 from django.contrib.auth.models import User
 from .models import Todo
 from rest_framework import serializers
 
+
+# Create User Serial for TodoSerializer
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Todo
-        fields = ['id','title', 'task', 'created_at']
+        fields = ['id','title', 'user', 'task', 'created_at']
--- ToDoList/views.py
+++ ToDoList/views.py
@@ -1,6 +1,7 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, serializers
 from rest_framework import permissions
 from .models import Todo
 from .serializers import TodoSerializer
@@ -14,3 +15,12 @@ class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all().order_by('created_at')
     serializer_class = TodoSerializer
     permission_classes = [] #permissions.IsAuthenticated            Ist der User registriert kann er die Unterlagen sehen, dafür ist permission_classes
+
+    #for a Post
+    def create(self,request):
+        todo = Todo.objects.create(title = self.request.POST['title'],
+                                    task = self.request.POST['task'],
+                                    user = self.request.user,
+                                    )
+        serialized_obj = serializers.serialize('json', [todo, ])
+        return HTTPResponse(serialized_obj, content_type='application/json')
